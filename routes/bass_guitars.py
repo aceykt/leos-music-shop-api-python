04e0f33cdec3e084c08578b6d478ebca02ed3dc6
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-09-28 16:25:34 -0700

    Joined load example.

--- routes/bass_guitars.py
+++ routes/bass_guitars.py
@@ -1,6 +1,6 @@
+from http.client import HTTPException
 from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
-from typing import Union
+from sqlalchemy.orm import joinedload, Session
 
 from dependencies.database import get_db
 from dependencies import models
@@ -16,6 +16,14 @@ async def get_all_bass_guitars(db: Session = Depends(get_db)):
     bass_guitars = db.query(models.BassGuitar).all()
     return bass_guitars
 
-@router.get("/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"id": item_id, "q": q}
+@router.get("/{id}")
+def read_item(id: int, db: Session = Depends(get_db)):
+    bass_guitar = db.query(models.BassGuitar) \
+        .options(joinedload(models.BassGuitar.manufacturer)) \
+        .filter(models.BassGuitar.id == id) \
+        .one_or_none()
+
+    if bass_guitar is None:
+        raise HTTPException(status_code=404, detail=f"Bass Guitar with Id {id} not found.")
+
+    return bass_guitar
